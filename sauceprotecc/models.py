36ab9fb79b357a3dd51fbb5f8ab5367b4e764753
Erika <erika.maust@gmail.com>
2020-01-01 13:02:51 -0800

    Created Uer, Image, and Flagged models

--- sauceprotecc/models.py
+++ sauceprotecc/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=100)
+    avatar = models.CharField(max_length=300, null=True, blank=True)
+    name = models.CharField(max_length=300, null=True, blank=True)
+    email = models.EmailField(max_length=300, null=True, blank=True)
+    social_media = models.CharField(max_length=400, null=True, blank=True)
+
+    class Meta:
+        ordering = ["username", "name", "email", "social_media"]
+
+    def __str__(self):
+        return 
+
+class Image(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    file_name = models.CharField(max_length=300)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    date_posted = models.DateTimeField()
+    post_location = models.URLField(null=True, blank=True)
+    whitelist = models.URLField()
+
+    class Meta:
+        ordering = ["user", "description", "file_name", "date_posted", "whitelist"]
+
+    def __str__(self):
+        return self.file_name
+
+class Flagged(models.Model):
+    images = models.ManyToManyField(Image)
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
--- sourcelibrary/settings.py
+++ sourcelibrary/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'erika',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
