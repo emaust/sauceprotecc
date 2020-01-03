9924c80cf1de3d0e0844c967722d9f3c1a77d77f
Erika <erika.maust@gmail.com>
2020-01-03 00:57:59 -0800

    Added default datetime to date_posted in model, removed print statement from view

--- sauceprotecc/views.py
+++ sauceprotecc/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import forms
-# from django.views.generic.edit import CreateView
-from django.http import HttpResponseRedirect
-# from django.urls import 
+from django.http import HttpResponseRedirect 
 from sauceprotecc.forms import UploadForm
 from sauceprotecc.models import Image
 
 class UploadImage(forms.Form):
-# class upload()
+
     model = Image
     fields = '__all__'
 
@@ -16,13 +14,11 @@ def index(request):
     return HttpResponse("This string represents the content of the httpresponse")
 
 def upload(request):
-    # form = UploadForm()
-    # print(request.method)
+
     if request.method == 'POST':
         form = UploadForm(request.POST)
         if form.is_valid():
             form.save()
-            print(form)
             return redirect('/upload/')
 
     else:
